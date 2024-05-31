c28ac3b06c97a307a8c4757bdfd7a0c5ad4f7d7b
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-05-31 15:05:38 +0200

    finished

--- main.js
+++ main.js
@@ -13,3 +13,11 @@ if(navClose){
         navMenu.classList.remove('show-menu')
     })
 }
+
+/*============BLUR HEADER==========================*/
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                        : header.classList.remove('blur-header')
+}
+window.addEventListenerListener('scroll, blurHeader')
